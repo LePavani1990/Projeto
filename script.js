6e9f64592f6f43bdd875c3733d88582b81cf29ae
unknown <lepavani1990@gmail.com>
2023-09-20 18:42:21 -0300

    tada: initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function xxx() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  const img = document.querySelector("#profile img")
+
+  // Substituir a imagem
+  if (html.classList.contains("light")) {
+    // Se tiver light mode, adicionar a imagem light
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Foto do professor do curso no light1111")
+  } else {
+    // Se tiver dark mode, adicionar a imagem dark
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute("alt", "Foto do professor do curso no darkzera")
+  }
+}
